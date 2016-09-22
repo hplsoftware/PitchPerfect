8d582d1f8f92f6c4325a2c372ec38d0ad5bb52e6
HPLSoftware <rob.s@hplsoftware.com>
2016-09-21 22:22:59 -0600

    View controller and Multiple views section code

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet var recordLabel: UILabel!
 
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        print("record button was pressed")
+        recordLabel.text = "Record Button was pressed"
+        recordButton.isEnabled=false
+        stopRecordingButton.isEnabled=true
+    }
+    @IBAction func stopRecording(_ sender: AnyObject) {
+        recordButton.isEnabled=true
+        stopRecordingButton.isEnabled=false
+        recordLabel.text = "Tap to Record"
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled=false
+    }
+    
 }
 
